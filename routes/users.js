f4faeaf87ce986e51dd6bd2f7fa9cf63c1dbbb5b
Mohammad Bastaty <72391469+MhdBastaty@users.noreply.github.com>
2022-02-22 18:26:46 +0800

    Add authorized for Admin and User

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,29 @@
+const User = require('../models/User');
 const router = require('express').Router();
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
 
+//UPDATE
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+    }
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-router.get("/usertest", (req, res) => {
-    res.send("succsess");
-});
-
-router.post("/userposttest", (req, res) => {
-    const username = req.body.username;
-    console.log(username);
-    res.status(201).json(username)
-});
 module.exports = router;

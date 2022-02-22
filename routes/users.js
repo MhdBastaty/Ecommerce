7ad6a66272f2ae7668b9bdcd69fddf5ec63bb263
Mohammad Bastaty <72391469+MhdBastaty@users.noreply.github.com>
2022-02-22 20:54:20 +0800

    add statatistic route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const router = require('express').Router();
-const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken')
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken')
 
 //UPDATE
 
@@ -25,5 +25,72 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // Delete
+
+  router.delete("/:id", verifyTokenAndAuthorization, async (req, res) =>{
+        
+    try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted"); 
+        } catch (error) {
+            res.status(500).json(error)
+        }
+  });
+
+// GET the  user so only admin can get user 
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) =>{
+    try {
+        const user = await User.findById(req.params.id)
+        const {password, ...others} = user._doc; 
+        res.status(200).json(others)
+    } catch (error) {
+        
+    }
+});
+
+// Find all users 
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;                              // to add specific users number from URL
+    try {
+        const users = query
+        ? await user.find().sort ({_id: -1}).limit (5) : await User.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+// Statistics
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  
+    try {
+      const data = await User.aggregate([
+        { $match: { createdAt: { $gte: lastYear } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: 1},
+          },
+        },
+      ]);
+      res.status(200).json(data)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
+
 
 module.exports = router;
